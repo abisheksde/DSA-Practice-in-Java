035d1254977ab1a2d6d74f3795b65cc7ae6bd8c6
abisheksde <abisheksde@gmail.com>
2024-05-13 18:13:56 +0530

    Impl

---
+++ Game/Chess/MovingConditions.java
@@ -0,0 +1,39 @@
+package Game.Chess;
+
+public class MovingConditions {
+    public static boolean sepoyMoving(){
+        // Implement the Moving Logic for Elephant
+
+        return true;
+    }
+
+    public static boolean elephantMoving(){
+        // Implement the Moving Logic for Elephant
+
+        return true;
+    }
+
+    public static boolean horseMoving(){
+        // Implement the Moving Logic for Elephant
+
+        return true;
+    }
+
+    public static boolean ministerMoving(){
+        // Implement the Moving Logic & Conditions
+
+        return true;
+    }
+
+    public static boolean kingMoving(){
+        // Implement the Moving Logic & Conditions
+
+        return true;
+    }
+
+    public static boolean queenMoving(){
+        // Implement the Moving Logic & Conditions
+
+        return true;
+    }
+}
