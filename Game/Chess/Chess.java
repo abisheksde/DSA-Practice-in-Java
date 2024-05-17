4c9dfcbcc0045e838c443e89fbb8b5e759e35ea1
abisheksde <abisheksde@gmail.com>
2024-05-17 17:32:14 +0530

    Completed half

--- Game/Chess/Chess.java
+++ Game/Chess/Chess.java
@@ -12,13 +12,19 @@ public class Chess {
         int[] from = new int[2];
         int[] to = new int[2];
 
+        /// Modify the Code like below
+        /// Customer will Enter 24 instead of 2 & 4
+        /// But, We Split that 24 into 2 & 4, then insert it into an Array
+        /// Now, we should pass the as a parameter in GameController.movePiece(from,to)
+        /// here, from = [2,4]
+
         while (true) {
             System.out.print("Move From : ");
-            for(int i= 0; i<2; i++){
+            for (int i = 0; i < 2; i++) {
                 from[i] = scanner.nextInt();
             }
             System.out.println("Move To : ");
-            for(int i= 0; i<2; i++){
+            for (int i = 0; i < 2; i++) {
                 to[i] = scanner.nextInt();
             }
 
@@ -27,5 +33,5 @@ public class Chess {
             Board.displayBoard();
         }
     }
-    
+
 }
