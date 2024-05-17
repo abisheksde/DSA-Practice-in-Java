82d5a1958a64a22369978fec7dd5877cee344ea3
abisheksde <abisheksde@gmail.com>
2024-05-17 17:14:05 +0530

    Completed isAnyObject()'s Black Object Moves Above in Straight Line

--- Game/Chess/ObjectFinding.java
+++ Game/Chess/ObjectFinding.java
@@ -1,51 +1,53 @@
 package Game.Chess;
 
 public class ObjectFinding {
-    public static boolean isAnyObject(int[] from, int[] to){
+    public static boolean isAnyObject(int[] from, int[] to) {
         int fromI = from[0];
         int fromJ = from[1];
 
         int toI = to[0];
         int toJ = to[1];
 
-        //int result;
+        // int result;
 
-        //***ERROR : Try to Find the Program Flow Accurately. Because, after 2nd if-condition, it is returning the true, but not go out of the Method */
-        //***ERROR :  Above result variable cannot be accessible within the 1st & 2nd If-Condition*/
+        // ***ERROR : Try to Find the Program Flow Accurately. Because, after 2nd
+        // if-condition, it is returning the true, but not go out of the Method */
+        // ***ERROR : Above result variable cannot be accessible within the 1st & 2nd
+        // If-Condition*/
 
-        //If Black object Moves Above in Straight Line 
-        if(fromI>toI && fromJ==toJ){
-            for(int i=fromI;i>toI; i--){
-                if(Board.board[i][1] == 0){
-                    return true; // Object found
+        // If Black object Moves Above in Straight Line
+        if (fromI > toI && fromJ == toJ) {
+            boolean objectFound = false;
+            for (int i = fromI; i > toI; i--) {
+                if (Board.board[i][toJ] != 0) {
+                    objectFound = true;
+                    break; // Object found
                 }
- 
+
             }
-            return false; // No object found
-        } else{
-            return false; // If not moving above, return true
-        } 
+            return objectFound; // If Object Found --> true ///// If No object found -- false
+        } else {
+            return false;
+        }
 
-        //If White object moves Below in Straight Line
-        //If Black object Moves Right Hand Side in Straight Line 
-        //If Black object Moves Left Hand Side in Straight Line 
-        //If Black object Moves Cross (Above) in Right Hand Side Line 
-        //If Black object Moves Cross (Above)  in Left Hand Side Line 
-        //If Black object Moves Cross (Below) in Right Hand Side Line 
-        //If Black object Moves Cross (Below)  in Left Hand Side Line 
+        // If White object moves Below in Straight Line
+        // If Black object Moves Right Hand Side in Straight Line
+        // If Black object Moves Left Hand Side in Straight Line
+        // If Black object Moves Cross (Above) in Right Hand Side Line
+        // If Black object Moves Cross (Above) in Left Hand Side Line
+        // If Black object Moves Cross (Below) in Right Hand Side Line
+        // If Black object Moves Cross (Below) in Left Hand Side Line
 
-        
         /////// Consider that - If I rotate the Chess Board ///////
-        
-        
-        //If White object moves Above in Straight Line
-        //If White object moves Below in Straight Line
-        //If Black object Moves Right Hand Side in Straight Line 
-        //If Black object Moves Left Hand Side in Straight Line 
-        //If Black object Moves Cross (Above) in Right Hand Side Line 
-        //If Black object Moves Cross (Above)  in Left Hand Side Line 
-        //If Black object Moves Cross (Below) in Right Hand Side Line 
-        //If Black object Moves Cross (Below)  in Left Hand Side Line  
-        
-   }
+
+        // If White object moves Above in Straight Line
+        // If White object moves Below in Straight Line
+        // If Black object Moves Right Hand Side in Straight Line
+        // If Black object Moves Left Hand Side in Straight Line
+        // If Black object Moves Cross (Above) in Right Hand Side Line
+        // If Black object Moves Cross (Above) in Left Hand Side Line
+        // If Black object Moves Cross (Below) in Right Hand Side Line
+        // If Black object Moves Cross (Below) in Left Hand Side Line
+
+    }
 }
